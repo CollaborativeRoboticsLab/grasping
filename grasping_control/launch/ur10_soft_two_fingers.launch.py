95e16eca00cb513cb190eb3497c1f2e279d26aef
Kalana Ratnayake <kalanaratnayake95@gmail.com>
2026-06-18 16:16:18 +0000

    added use_demo launch parameter

--- grasping_control/launch/ur10_soft_two_fingers.launch.py
+++ grasping_control/launch/ur10_soft_two_fingers.launch.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition, UnlessCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
@@ -15,15 +16,14 @@ def generate_launch_description() -> LaunchDescription:
 		PythonLaunchDescriptionSource(
 			str(Path(ur10_moveit_config_share) / 'launch' / 'hardware_with_moveit.launch.py')
 		),
-		launch_arguments={
-			'ur_type': LaunchConfiguration('ur_type'),
-			'robot_ip': LaunchConfiguration('robot_ip'),
-			'reverse_ip': LaunchConfiguration('reverse_ip'),
-			'use_sim': LaunchConfiguration('use_sim'),
-			'launch_rviz': LaunchConfiguration('launch_rviz'),
-			'initial_joint_controller': LaunchConfiguration('initial_joint_controller'),
-			'activate_joint_controller': LaunchConfiguration('activate_joint_controller'),
-		}.items(),
+		condition=UnlessCondition(LaunchConfiguration('use_demo')),
+	)
+
+	demo = IncludeLaunchDescription(
+		PythonLaunchDescriptionSource(
+			str(Path(ur10_moveit_config_share) / 'launch' / 'demo.launch.py')
+		),
+		condition=IfCondition(LaunchConfiguration('use_demo')),
 	)
 
 	motion_execution_node = Node(
@@ -53,13 +53,7 @@ def generate_launch_description() -> LaunchDescription:
 
 	return LaunchDescription(
 		[
-			DeclareLaunchArgument('ur_type', default_value='ur10'),
-			DeclareLaunchArgument('robot_ip', default_value='192.168.10.156'),
-			DeclareLaunchArgument('reverse_ip', default_value='192.168.10.130'),
-			DeclareLaunchArgument('use_sim', default_value='false'),
-			DeclareLaunchArgument('launch_rviz', default_value='true'),
-			DeclareLaunchArgument('initial_joint_controller', default_value='scaled_joint_trajectory_controller'),
-			DeclareLaunchArgument('activate_joint_controller', default_value='true'),
+			DeclareLaunchArgument('use_demo', default_value='false'),
 			DeclareLaunchArgument('arm_action_name', default_value='move_arm_to_pose'),
 			DeclareLaunchArgument('move_group_action_name', default_value='move_action'),
 			DeclareLaunchArgument('planning_group', default_value='manipulator'),
@@ -74,6 +68,7 @@ def generate_launch_description() -> LaunchDescription:
 			DeclareLaunchArgument('planning_pipeline_id', default_value=''),
 			DeclareLaunchArgument('planner_id', default_value=''),
 			DeclareLaunchArgument('workspace_config_path', default_value=''),
+			demo,
 			hardware_with_moveit,
 			motion_execution_node,
 		]
